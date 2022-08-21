30d27eaf6351a977e856b93e5b5c6bc2f09d1a99
freol35241 <freol@outlook.com>
2022-08-21 19:56:57 +0200

    Reverting logging library (see https://github.com/hideakitai/DebugLog/issues/6) and adding finishing touches before deploy.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <Ticker.h>
 #include <AsyncMqttClient.h>
 #include <ReactESP.h>
-#include <DebugLog.h>
 
 #include "config.h"
 
@@ -17,51 +16,42 @@ Ticker wifiReconnectTimer;
 
 void connectToWifi()
 {
-  LOG_INFO("Connecting to Wi-Fi...");
+  Serial.println("Connecting to Wi-Fi...");
   WiFi.begin(WIFI_SSID, WIFI_PW);
 }
 
 void connectToMqtt()
 {
-  LOG_INFO("Connecting to MQTT...");
+  Serial.println("Connecting to MQTT...");
   mqttClient.connect();
 }
 
 void onWifiConnect(const WiFiEventStationModeGotIP &event)
 {
-  LOG_INFO("Connected to Wi-Fi.");
+  Serial.println("Connected to Wi-Fi.");
   connectToMqtt();
 }
 
 void onWifiDisconnect(const WiFiEventStationModeDisconnected &event)
 {
-  LOG_INFO("Disconnected from Wi-Fi.");
+  Serial.println("Disconnected from Wi-Fi.");
   mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
   wifiReconnectTimer.once(2, connectToWifi);
 }
 
 void onMqttConnect(bool sessionPresent)
 {
-  LOG_INFO("Connected to MQTT.");
-  LOG_DEBUG("Session present: ");
-  LOG_DEBUG(sessionPresent);
+  Serial.println("Connected to MQTT.");
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
 {
-  LOG_INFO("Disconnected from MQTT.");
+  Serial.println("Disconnected from MQTT.");
 
   if (WiFi.isConnected())
   {
     mqttReconnectTimer.once(2, connectToMqtt);
   }
-}
-
-void onMqttPublish(uint16_t packetId)
-{
-  LOG_DEBUG("Publish acknowledged.");
-  LOG_DEBUG("  packetId: ");
-  LOG_DEBUG(packetId);
 }
 
 void setup()
@@ -78,17 +68,14 @@ void setup()
   // Configure mqtt client
   mqttClient.onConnect(onMqttConnect);
   mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onPublish(onMqttPublish);
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
   mqttClient.setCredentials(MQTT_USER, MQTT_PW);
 
   // Connect to wifi (and subsequently mqtt)
   connectToWifi();
 
-  // Pin config
+  Serial.println("Setting up pin config and attaching callback");
   pinMode(INTERRUPT_PIN_NUMBER, INPUT_PULLUP);
-
-  // Attach pin interrupt
   app.onInterrupt(INTERRUPT_PIN_NUMBER, FALLING, []()
                   {
                     static unsigned long _previous = 0L;
@@ -96,37 +83,48 @@ void setup()
 
                     auto now = millis();
 
-                    auto dt = now - _previous;
-
                     if (_previous == 0L)
                     {
-                      LOG_DEBUG("First falling edge detected, next one will start generating data.");
+                      Serial.println("First falling edge detected, next one will start generating data.");
                       _previous = now;
                       return;
                     }
+
+                    auto dt = now - _previous;
                     _previous = now;
+
+                    Serial.print("Delta (ms): ");
+                    Serial.println(dt);
 
                     if (dt < DEBOUNCE_TIME)
                     {
-                      LOG_DEBUG("Detection debounced...");
+                      Serial.println("Detection debounced...");
                       return;
                     }
 
                     _counter++;
-                    LOG_INFO("Falling edge (", _counter, ") detected!");
+                    Serial.print("Falling edge (");
+                    Serial.print(_counter);
+                    Serial.println(") detected!");
 
-                    float power = (1000 / BLINKS_PER_KWH) / (float)((dt / 1000) / 3600);
-                    LOG_INFO("Power: ", power, " W");
+                    float power = (1000 / BLINKS_PER_KWH) / (((float)dt / 1000) / 3600);
+                    Serial.print("Power: ");
+                    Serial.print(power);
+                    Serial.println(" W");
 
+                    if (mqttClient.connected())
+                    {
+                      Serial.println("Publishing to MQTT broker");
+                      // Publish power
+                      mqttClient.publish((MQTT_BASE_TOPIC + String("/power")).c_str(), 0, false, String(power).c_str());
 
-                    // Publish power
-                    mqttClient.publish((MQTT_BASE_TOPIC + String("/power")).c_str(), 0, false, String(power).c_str());
-
-                    // Publish incrementer
-                    mqttClient.publish((MQTT_BASE_TOPIC + String("/counter")).c_str(), 0, false, String(_counter).c_str()); });
+                      // Publish incrementer
+                      mqttClient.publish((MQTT_BASE_TOPIC + String("/counter")).c_str(), 0, false, String(_counter).c_str());
+                    } else {
+                      Serial.println("MQTT not connected, skipping publishing...");
+                    } });
 }
 
 void loop()
 {
-  app.tick();
 }
