7b13c0b534e44addc329f0b82883f7b2ea03d86a
freol35241 <freol@outlook.com>
2022-10-30 14:17:30 +0100

    Reintroducing debuglog using fixed, unofficial branch of DebugLog

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <AsyncMqttClient.h>
 #include <ReactESP.h>
 #include <ArduinoJson.h>
+#include <DebugLog.h>
 
 #include "HAN.h"
 
@@ -19,37 +20,37 @@ Ticker wifiReconnectTimer;
 
 void connectToWifi()
 {
-  Serial.println("Connecting to Wi-Fi...");
+  LOG_INFO("Connecting to Wi-Fi...");
   WiFi.begin(WIFI_SSID, WIFI_PW);
 }
 
 void connectToMqtt()
 {
-  Serial.println("Connecting to MQTT...");
+  LOG_INFO("Connecting to MQTT...");
   mqttClient.connect();
 }
 
 void onWifiConnect(const WiFiEventStationModeGotIP &event)
 {
-  Serial.println("Connected to Wi-Fi.");
+  LOG_INFO("Connected to Wi-Fi.");
   connectToMqtt();
 }
 
 void onWifiDisconnect(const WiFiEventStationModeDisconnected &event)
 {
-  Serial.println("Disconnected from Wi-Fi.");
+  LOG_INFO("Disconnected from Wi-Fi.");
   mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
   wifiReconnectTimer.once(2, connectToWifi);
 }
 
 void onMqttConnect(bool sessionPresent)
 {
-  Serial.println("Connected to MQTT.");
+  LOG_INFO("Connected to MQTT.");
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
 {
-  Serial.println("Disconnected from MQTT.");
+  LOG_INFO("Disconnected from MQTT.");
 
   if (WiFi.isConnected())
   {
@@ -75,10 +76,10 @@ void setup()
   U0C0 |= BIT(UCRXI);          // Inverse RX
   Serial.setRxBufferSize(512); // Make sure buffer is large enough for us to have time for the processing
 
-  Serial.println("Setting up serial callback");
+  LOG_INFO("Setting up serial callback");
   app.onAvailable(Serial, []()
                   {
-                    Serial.println("Data available on Serial");
+                    LOG_INFO("Data available on Serial");
 
                     HAN::Telegram telegram = HAN::receive_telegram(Serial);
 
@@ -96,19 +97,18 @@ void setup()
 
                     String output;
                     serializeJson(doc, output);
-                    Serial.println("Serialized telegram:");
-                    Serial.println(output);
+                    LOG_DEBUG("Serialized telegram:", output);
 
                     // Publish to mqtt
                     if (mqttClient.connected())
                     {
-                      Serial.println("Publishing to MQTT broker");
+                      LOG_INFO("Publishing to MQTT broker");
 
                       mqttClient.publish(String(MQTT_BASE_TOPIC).c_str(), 0, false, output.c_str());
                     }
                     else
                     {
-                      Serial.println("MQTT not connected, skipping publishing...");
+                      LOG_WARN("MQTT not connected, skipping publishing...");
                     } }
 
   );
